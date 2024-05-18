590e39ac3b36721a044e43db2841d386f982e3b3
Sneha Kumari <siddharth8271skg2@gmail.com>
2024-05-18 15:25:05 +0530

    file upload using dropzone introduced

--- src/app/configure/upload/page.tsx
+++ src/app/configure/upload/page.tsx
@@ -1,11 +1,44 @@
-import React from 'react'
+'use client'
+import { cn } from "@/lib/utils";
+import React, { useState } from "react";
+import Dropzone from "react-dropzone";
 
-type Props = {}
+type Props = {};
 
 const page = (props: Props) => {
-  return (
-    <div>page</div>
-  )
-}
+  const [isDragOver, setIsDragOver] = useState<boolean>(false);
+  const onDropAccepted = () => {
+    console.log('accepted')
+  };
+  const onDropRejected = () => {};
 
-export default page
+  return (
+    <div className={cn(
+      'relative h-full flex-1 my-16 w-full rounded-xl bg-gray-900/5 p-2 ring-1 ring-inset ring-gray-900/10 lg:rounded-2xl flex justify-center flex-col items-center',
+      {
+        'ring-blue-900/25 bg-blue-900/10': isDragOver,
+      }
+    )}>
+      <Dropzone
+        onDropAccepted={onDropAccepted}
+        onDropRejected={onDropRejected}
+        onDragEnter={() => setIsDragOver(true)}
+        onDragLeave={() => setIsDragOver(false)}
+        accept={{
+          "image/jpeg": [".jpeg"],
+          "image/png": [".png"],
+          "image/jpg": [".jpg"],
+        }}
+      >
+        {({ getRootProps, getInputProps }) => (
+          <div {...getRootProps()}>
+            <input {...getInputProps()} />
+            <p>Drag 'n' drop some files here, or click to select files</p>
+          </div>
+        )}
+      </Dropzone>
+    </div>
+  );
+};
+
+export default page;
